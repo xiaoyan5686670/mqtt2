bc3870012185414a7028ad2475593c72af7c6a35
秦晓琰 <xiaoyan5686670@163.com>
2025-12-30 15:28:04 +0800

    Revert axios baseURL to correct backend port 8000

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 
 // 配置axios基础URL
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:8080'  // 后端API运行在8080端口
+axios.defaults.baseURL = 'http://localhost:8000'  // 后端API运行在8000端口
 
 // 创建Vue应用
 const app = createApp(App)
