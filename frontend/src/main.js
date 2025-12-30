b5a986950d20de3405f960905081149dc6328eec
秦晓琰 <xiaoyan5686670@163.com>
2025-12-30 15:22:02 +0800

    Fix MQTT config save issue by updating axios baseURL to correct backend port

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,13 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-
-// 导入Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-// 移除JS导入，改为在index.html中引入
+import { createPinia } from 'pinia'
 
 // 配置axios基础URL
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'http://localhost:8080'  // 后端API运行在8080端口
 
+// 创建Vue应用
 const app = createApp(App)
 
 app.use(createPinia())
